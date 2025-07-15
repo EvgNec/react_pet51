6cb80a4865ad4a30752cbfebe09fe91d3f9d9882
Evgeniy <evgnec@gmail.com>
2025-07-16 00:57:33 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import Calc from "./Calc/Calc";
+import Player from "./Player/Player";
 import UserMenu from "./UseContext/UserMenu";
 
 export const App = () => {
@@ -6,6 +7,7 @@ export const App = () => {
     <div>
     <UserMenu/>
     <Calc startQty={5}/>
+    <Player source="<http://media.w3.org/010/05/sintel/trailer.mp4>" /> 
     </div>
   );
 };
